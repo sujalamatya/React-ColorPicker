6375eb1ea0390c1c60dbe7595a92b9cdec9d7d6e
sujal amatya <sujalcr10@gmail.com>
2024-10-20 12:41:26 +0545

    color picker final

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import Button from "./Button.jsx";
+import ColorPicker from './colorPicker.jsx'
+function App() {
+  return(
+    <>
+    <ColorPicker/>
+    </>
+  );
+  
+}
+
+export default App
